8c136d9813a5e8046df1e73639005a006ff60ea9
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-08-22 15:27:04 +0200

    feat: Add wheel and build script

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from os.path import dirname, join
+from setuptools import find_packages, setup
+from src import opex_dashboard
+
+description_file = join(dirname(__file__), "README.md")
+
+setup(
+    name=opex_dashboard.__title__,
+    author=opex_dashboard.__author__,
+    url="https://github.com/pagopa/operational-excellence-dashboard",
+    project_urls={
+        "Source": "https://github.com/pagopa/operational-excellence-dashboard",
+    },
+    version=opex_dashboard.__version__,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    description=opex_dashboard.__description__,
+    long_description=open(description_file).read(),
+    long_description_content_type="text/markdown",
+    keywords="openapi3, swagger, python",
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=[
+        # "prance>=0.20.2",
+        # "openapi-spec-validator>=0.2.9",
+    ],
+)
---
+++ src/opex_dashboard/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.1'
+__title__ = 'operational-excellence-dashboard'
+__author__ = '@pagopa/infrastructure-admins'
+__description__ = 'A cli to generate PagoPA\'s Operational Excellence dashboards from open api specs'
+
