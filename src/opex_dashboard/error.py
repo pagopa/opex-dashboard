f2e9b1ba4eb3dbfec508fdfd9f6350ebacdbbcfd
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-08-24 15:38:22 +0200

    feat: Develop builder

--- src/opex_dashboard/error.py
+++ src/opex_dashboard/error.py
@@ -3,3 +3,15 @@ class ParseError(Exception):
     Throw when a parsing error occurs
     """
     pass
+
+class RenderError(Exception):
+    """
+    Throw when it is impossible to render a template
+    """
+    pass
+
+class PlaceholderError(Exception):
+    """
+    Throw when a substitution has no placeholder to replace
+    """
+    pass
