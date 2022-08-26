89505ebeda436a461447ef7e78860ddff47f27ad
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-08-26 18:10:10 +0200

    refactor: Improve custom tags and document them

--- src/opex_dashboard/tags/mul.py
+++ src/opex_dashboard/tags/mul.py
@@ -3,6 +3,11 @@ from django import template
 register = template.Library()
 
 @register.filter
-def mul(value, factor):
+def mul(value: int, factor: int) -> int:
+    """Multiply an integer with a factor
+
+    Returns:
+        int -> The prodict object
+    """
     return value * factor;
 
--- src/opex_dashboard/tags/stringify.py
+++ src/opex_dashboard/tags/stringify.py
@@ -1,7 +1,15 @@
+import re
+
 from django import template
 
 register = template.Library()
 
 @register.filter
-def stringify(value):
-    return repr(value).replace("'", "\"")
+def stringify(value: object) -> str:
+    """Serialize an object in a string and wrap it inside double quote
+
+    Returns:
+        str -> The serialized object
+    """
+    escaped = re.sub("(?<!\\\\)\"", "\\\"", repr(value))
+    return escaped.replace("'", "\"")
