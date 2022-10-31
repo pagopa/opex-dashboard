d5d57f9fa0ca76e10fa08cf7b0ca72ada8465544
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-10-31 09:26:21 +0100

    fix: Change "other" value behavior

--- src/opex_dashboard/assets/terraform/terraform.sh
+++ src/opex_dashboard/assets/terraform/terraform.sh
@@ -5,7 +5,7 @@ set -e
 ACTION=$1
 ENV=$2
 shift 2
-other="$*"
+other="$@"
 # must be subscription in lower case
 subscription=""
 BACKEND_CONFIG_PATH="./.env/${ENV}/backend_state.tfvars"
@@ -44,23 +44,18 @@ export HELM_DEBUG=1
 if echo "init plan apply refresh import output state taint destroy" | grep -w "$ACTION" > /dev/null; then
   if [ "$ACTION" = "init" ]; then
     echo "[INFO] init tf on ENV: ${ENV}"
-    terraform "$ACTION" -backend-config="${BACKEND_CONFIG_PATH}" "$other"
+    terraform "$ACTION" -backend-config="${BACKEND_CONFIG_PATH}" $other
   elif [ "$ACTION" = "output" ] || [ "$ACTION" = "state" ] || [ "$ACTION" = "taint" ]; then
     # init terraform backend
     terraform init -reconfigure -backend-config="${BACKEND_CONFIG_PATH}"
-    terraform "$ACTION" "$other"
+    terraform "$ACTION" $other
   else
     # init terraform backend
     echo "[INFO] init tf on ENV: ${ENV}"
-    terraform init \
-    -reconfigure \
-    -backend-config="${BACKEND_CONFIG_PATH}"
+    terraform init -reconfigure -backend-config="${BACKEND_CONFIG_PATH}"
 
     echo "[INFO] run tf with: ${ACTION} on ENV: ${ENV} and other: >${other}<"
-    terraform "${ACTION}" \
-    -var-file="./.env/${ENV}/terraform.tfvars" \
-    -compact-warnings \
-    $other
+    terraform "${ACTION}" -var-file="./.env/${ENV}/terraform.tfvars" -compact-warnings $other
   fi
 else
     echo "[ERROR] ACTION not allowed."
